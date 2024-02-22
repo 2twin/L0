069b8686b68a10fe0b1ba65b548e616fc8b2c134
Ilya Vladimirov <vl.ilya@mail.ru>
2024-02-22 22:42:58 +0300

    базовая архитектура проекта

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/2twin/L0/internal/app"
+)
+
+func main() {
+	ctx := context.Background()
+
+	a, err := app.NewApp(ctx)
+	if err != nil {
+		log.Fatalf("failed to init app: %s", err.Error())
+	}
+
+	err = a.Run()
+	if err != nil {
+		log.Fatalf("failed to run app: %s", err.Error())
+	}
+}
